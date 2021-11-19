a56ea536800c69de44b6c973f17517778ac3b3ef
jhshin29 <shinji03264@gmail.com>
2021-11-19 16:10:51 +0900

    Initial commit

---
+++ src/main/java/com/snp/takealook/TakealookApplication.java
@@ -0,0 +1,13 @@
+package com.snp.takealook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakealookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TakealookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/snp/takealook/TakealookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snp.takealook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakealookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
