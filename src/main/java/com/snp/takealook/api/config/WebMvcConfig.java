07d92f35a69c92a97d0f0b27f8a65be3081cd956
jhshin29 <shinji03264@gmail.com>
2021-12-08 18:06:29 +0900

    [Fix] Config

--- src/main/java/com/snp/takealook/api/config/WebMvcConfig.java
+++ src/main/java/com/snp/takealook/api/config/WebMvcConfig.java
@@ -1,4 +1,4 @@
-package com.snp.takealook.config;
+package com.snp.takealook.api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
