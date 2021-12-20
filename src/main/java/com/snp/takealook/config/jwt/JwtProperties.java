c005ce3760580b2ebd8c44d3cff6faae1f787718
geesuee <baejisu97@gmail.com>
2021-12-20 18:03:36 +0900

    [Fix] Token Expire_time

--- src/main/java/com/snp/takealook/config/jwt/JwtProperties.java
+++ src/main/java/com/snp/takealook/config/jwt/JwtProperties.java
@@ -3,7 +3,7 @@ package com.snp.takealook.config.jwt;
 public interface JwtProperties {
 
     String SECRET = "java";
-    Integer EXPIRE_TIME = 1000 * 60 * 60; //1시간
+    Integer EXPIRE_TIME = 1000 * 60 * 60 * 24; // 하루
     String TOKEN_PRIFIX = "Bearer ";
     String TOKEN_HAEDER = "Authorization";
 
