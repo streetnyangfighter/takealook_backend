8e1fcd509657365ae988614b313940af1679b023
geesuee <baejisu97@gmail.com>
2021-12-22 02:24:25 +0900

    [Fix] Cat Recommendation Except User's cat

--- src/main/java/com/snp/takealook/api/service/cat/CatService.java
+++ src/main/java/com/snp/takealook/api/service/cat/CatService.java
@@ -28,13 +28,16 @@ public class CatService {
     private final S3Uploader s3Uploader;
 
     @Transactional(readOnly = true)
-    public List<ResponseDTO.CatRecommendListResponse> findRecommendCats(double latitude, double longitude) {
+    public List<ResponseDTO.CatRecommendListResponse> findRecommendCats(Long userId, double latitude, double longitude) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("User with id: " + userId + " is not valid"));
         List<CatLocation> locationList = catLocationRepository.findNearCatLocations(latitude, longitude);
         System.out.println("********111 " + locationList.size());
 
         Set<Cat> catSet = new HashSet<>();
         for (CatLocation location : locationList) {
-            if (!location.getSelection().getCat().getDflag() && !location.getSelection().getCat().getAflag()) {
+            if (!location.getSelection().getCat().getDflag()
+                    && !location.getSelection().getCat().getAflag()
+                    && location.getSelection().getUser() != user) {
                 catSet.add(location.getSelection().getCat());
             }
         }
@@ -43,8 +46,8 @@ public class CatService {
         List<ResponseDTO.CatRecommendListResponse> result = new ArrayList<>();
         Iterator<Cat> iterator = catSet.iterator();
         while (iterator.hasNext()) {
-            List<CatLocation> recentLocationList = new ArrayList<>();
             Cat cat = iterator.next();
+            List<CatLocation> recentLocationList = new ArrayList<>();
             List<Selection> sameCatSelectionList = selectionRepository.findSelectionsByCat(cat);
             for (Selection selection : sameCatSelectionList) {
                 recentLocationList.addAll(selection.getCatLocationList());
